2e75f8b33208f338061d2090da3c0ccce2da5661
sergioG1812 <sergioa.gonzaleza@outlook.com>
2025-05-30 16:07:20 -0500

    Add files via upload

---
+++ Juego (1).h
@@ -0,0 +1,28 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+using namespace std;
+#include <vector>
+#include "Jugador.h"
+#include "Baraja.h"
+#include "Tablero.h"
+
+class Juego {
+public:
+    Juego(int numJugadores);
+    void iniciar();
+    void mostrarEstado();
+    void finalizarJuego();
+
+private:
+    void turnoJugador(int jugadorId);
+    bool verificarFinJuego();
+
+    int numJugadores;
+    int jugadorActual;
+    bool juegoTerminado;
+    vector<Jugador> jugadores;
+    Baraja baraja;
+    Tablero tablero;
+};
+
+#endif
